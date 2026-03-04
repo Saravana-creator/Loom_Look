a76c6b33c5352dedad29cbcc858f2782d98130d0
Saravana-creator <saravana24057@gmail.com>
2026-03-04 15:33:05 +0530

    commit

--- frontend/src/pages/auth/VendorRegisterPage.js
+++ frontend/src/pages/auth/VendorRegisterPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { authService } from '../../services';
 import { Spinner } from '../../components/common/UI';
 import { toast } from 'react-toastify';
